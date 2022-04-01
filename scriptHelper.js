58f4066098056a6af5244dff9f988f7b39841f0c
LucasPBernard <lucas.p.bernard@gmail.com>
2022-04-01 02:52:54 -0500

    debugging 4

--- scriptHelper.js
+++ scriptHelper.js
@@ -62,6 +62,7 @@ function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
         launchStatus.setAttribute('style','color:rgb(65,159,106)');
         launchStatus.innerHTML = "Shuttle is Ready for Launch";
         fuelStatus.innerHTML = "Fuel level high enough for launch";
+        cargoStatus.innerHTML = "Cargo mass low enough for launch";
     } 
  
 }
